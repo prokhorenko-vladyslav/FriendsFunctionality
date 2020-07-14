76de2efef44091e0c9c87adb1cbf4106691bd203
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-07-14 23:05:34 +0300

    Added auth and friend routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->middleware('force_json')->group(function() {
+    Route::prefix('auth')->namespace('Auth')->group(function () {
+        Route::post('register', 'AuthController@register');
+        Route::post('login', 'AuthController@login')->name('login');
+    });
+
+    Route::middleware('auth:api')->group(function() {
+        Route::prefix('crm')->namespace('CRM')->group(function() {
+            Route::apiResource('friend', 'FriendController');
+        });
+    });
 });
